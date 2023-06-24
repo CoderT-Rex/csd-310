0245e368ac825ff40ad69f29307d59a1565dbc08
CoderT-Rex <joshua_rex@rocketmail.com>
2023-06-24 13:33:26 -0400

    Added Module 6 test file to the repository

---
+++ module_6/mysql_test.py
@@ -0,0 +1,37 @@
+#Joshua Rex
+#Database Developement and Use 
+#Module 6.2 Assignment
+#6/19/2023
+
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+	"user": "root",
+	"password": "Purplepig92",
+	"host": "127.0.0.1",
+	"database": "movies",
+	"raise_on_warnings": True
+}
+db = None  # Define db variable outside the try block
+
+try:
+	db = mysql.connector.connect(**config)
+
+	print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+	input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("  The supplied username or password are invalid")
+
+	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("   The specified database does not exist")
+
+	else:
+		print(err)
+
+finally:
+	if db is not None:
+		db.close()
